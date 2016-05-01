d15e0a14d592ebf3277a3913b9c9f236ba4fbc86
seriokilla <seriokilla187@gmail.com>
2016-05-01 19:54:25 -0400

    added page refresh after we add new line item

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,16 +1,21 @@
 function AppCtrl($scope, $http){
     console.log("hello from AppCtrl");
     
-    $http.get("/contactlist").success(function(resp){
-        console.log("got data");
-        $scope.contactlist = resp;
-    });
+    var refresh = function(){
+        $http.get("/contactlist").success(function(resp){
+            console.log("got data");
+            $scope.contactlist = resp;
+            $scope.contact = "";
+        });
+    };
     
+    refresh();
     
     $scope.addContact = function(){
         console.log($scope.contact);
         $http.post("/contactlist", $scope.contact).success(function(resp){
             console.log(resp);
+            refresh();
         });
         
     };
