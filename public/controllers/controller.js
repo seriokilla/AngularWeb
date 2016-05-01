b627169477abde87fb6aa64b0157d6d24dbf4447
seriokilla <seriokilla187@gmail.com>
2016-05-01 15:58:47 -0400

    moved data to server.js to return array. added http get in controller so it can call server to get data.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,22 +1,8 @@
-function AppCtrl($scope){
+function AppCtrl($scope, $http){
     console.log("hello from AppCtrl");
     
-    person1 = {
-      name: "name001",
-      email: "email001",
-      number: "111-111-1111"  
-    };
-    person2 = {
-      name: "name002",
-      email: "email002",
-      number: "222-222-2222"  
-    };
-    person3 = {
-      name: "name003",
-      email: "email003",
-      number: "333-333-3333"  
-    };
-    
-    var contactlist = [person1, person2, person3];
-    $scope.contactlist = contactlist;
+    $http.get("/contactlist").success(function(resp){
+        console.log("got data");
+        $scope.contactlist = resp;
+    });
 }
--- server.js
+++ server.js
@@ -10,7 +10,27 @@ var app = express();
 
 // use static pages from location
 app.use(express.static(__dirname + "/public"));
-
+app.get("/contactlist", function(req, res){
+    console.log("rcved get request");
+    person1 = {
+      name: "name001",
+      email: "email001",
+      number: "111-111-1111"  
+    };
+    person2 = {
+      name: "name002",
+      email: "email002",
+      number: "222-222-2222"  
+    };
+    person3 = {
+      name: "name003",
+      email: "email003",
+      number: "333-333-3333"  
+    };
+    
+    var contactlist = [person1, person2, person3];
+    res.json(contactlist);
+});
 
 app.listen(3000);
 console.log("server running on port 3000");
