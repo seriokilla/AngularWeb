cae6b88d78597f3585e77d3d13ca316b6aad4a40
seriokilla <seriokilla187@gmail.com>
2016-05-01 16:07:15 -0400

    moved json into separate file and made server load file when request is made.

--- server.js
+++ server.js
@@ -12,23 +12,8 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 app.get("/contactlist", function(req, res){
     console.log("rcved get request");
-    person1 = {
-      name: "name001",
-      email: "email001",
-      number: "111-111-1111"  
-    };
-    person2 = {
-      name: "name002",
-      email: "email002",
-      number: "222-222-2222"  
-    };
-    person3 = {
-      name: "name003",
-      email: "email003",
-      number: "333-333-3333"  
-    };
-    
-    var contactlist = [person1, person2, person3];
+      
+    var contactlist = require("./data/contacts.json");
     res.json(contactlist);
 });
 
